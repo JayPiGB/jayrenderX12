1c351829c5131fb718e7990188072e3b4e007521
JayPiGB <jp.borgneth@gmail.com>
2026-05-12 12:12:26 -0300

    add standard include header files for windows and d3d12 related

---
+++ src/std_includes/std_includes.h
@@ -0,0 +1,14 @@
+#define UNICODE
+#include <windows.h>
+#include <shellapi.h>
+#include <wrl.h>
+using namespace Microsoft::WRL;
+
+#include <directx/d3dx12.h>
+
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+
+
